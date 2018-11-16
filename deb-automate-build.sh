c0c4fd0c9edb410d344cf5d74e506d338b943697
ctrix <mc@spamteq.com>
2018-11-16 17:41:16 +0000

    Small fixes

--- deb-automate-build.sh
+++ deb-automate-build.sh
@@ -8,12 +8,13 @@ sudo apt-get -y install dpkg-dev debhelper dh-systemd devscripts libwww-perl
 
 rm -f ../minio.RELEASE.*
 rm -f ./minio
-uscan --debug -v --no-symlink --download-current-version
+uscan --no-symlink --download-current-version
 
 FILE=`ls -1 ../minio.RELEASE.*Z`
 mv $FILE ./minio
 chmod 755 ./minio
 chown root:root ./minio
 
-#dpkg-buildpackage -us -uc -b
+dpkg-buildpackage -us -uc -b
 debian/rules clean
+rm -f minio
